eaeb121fbed4bb4709b4edda906c4706ca7e2488
Andrea Lombardo <andrea.lombardo@enel.com>
2024-01-19 19:57:01 +0100

    Update styles.css and main.js, add contact.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,27 @@
 // Code JavaScript for responsive menu
 document.addEventListener('DOMContentLoaded', function() {
     var menuBtn = document.querySelector('.menu-btn');
+    var slides = document.querySelectorAll('.video-slide');
     var navigation = document.querySelector('.navigation');
-    var btns = document.querySelectorAll('.nav.btn');
+    var btns = document.querySelectorAll('.nav-btn');
+    var contents = document.querySelectorAll('.content ');
     var sliderNav = function(manual){
-        btns.forEach(function(btn){
+        btns.forEach((btn)=>{
             btn.classList.remove('active');
-        })
+        });
+        slides.forEach((slide)=>{
+            slide.classList.remove('active');
+        });
+        contents.forEach((content)=>{
+            content.classList.remove('active');
+        });
         btns[manual].classList.add('active');
+        slides[manual].classList.add('active');
+        contents[manual].classList.add('active');
+
     }
-    btns.forEach(function(btn, i){
-        btn.addEventListener('click', function(){
-            
+    btns.forEach((btn, i)=>{
+        btn.addEventListener('click', ()=>{
             sliderNav(i);
         })
     })
